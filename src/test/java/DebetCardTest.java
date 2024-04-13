90589ab9818bc0f4bfa992845d81b24b68fa0193
Nikita Spiridonov <spiridonov.n.i@yandex.ru>
2024-04-13 16:40:43 +0300

    Change code

--- src/test/java/DebetCardTest.java
+++ src/test/java/DebetCardTest.java
@@ -1,2 +1,24 @@
-public class DebetCardTest {
+
+import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.Test;
+
+
+import java.util.List;
+
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+class DebetCardTest {
+
+    @Test
+    public void shouldCreateNewForm() {
+        open("http://localhost:9999");
+        SelenideElement form = $("[data-test-id=callback-form]");
+        $("[data-test-id='name'] input").setValue("Никита Спиридонов");
+        $("[data-test-id='phone'] input").setValue("+79250561286");
+        $("[data-test-id='agreement']").click();
+        $("button.button").click();
+        $("[data-test-id='order-success']").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+    }
 }
+
